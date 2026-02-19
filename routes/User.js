4da5715b633ed477df5d9b6ae42f8e9cf4ec4312
Edward Anderson <edward.a93@hotmail.co.uk>
2026-02-19 19:46:04 +0100

    Added user.js and adjusted routes

---
+++ routes/User.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/users");
+
+router.post("/create", async (req, res) => {
+    try {
+        const user = await User.create(req.body);
+        res.status(201).send({ message: "User successfully created", user });
+    } catch (error) {
+        res.status(500).send({ message: "Error creating user" });
+    }
+});
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const tasksRoutes = require('./tasks');
 
-router.use('/', tasksRoutes);
+const tasksRoutes = require('./tasks');
+const usersRoutes = require('./User'); 
+
+router.use('/tasks', tasksRoutes); 
+router.use('/users', usersRoutes); 
 
 module.exports = router;
